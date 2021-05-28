29932fdd064e731f85ec507a35f1d666365abfef
Hugo <72490259+hu9okwan@users.noreply.github.com>
2021-05-27 17:59:16 -0700

    Removes console.log in auth_controller

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -55,7 +55,6 @@ let authController = {
         let user = await userController.registerUser(name, email, password)
 
         
-        console.log(user)
         if (user) {
             
             const hash = createHash('sha256');
